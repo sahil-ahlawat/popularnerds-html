132cd9f6ea8be98b7f7dcf00f3560644fdd3bb8f
sahildevil <sahil.a@konsultapp.com>
2018-04-20 15:55:34 +0530

    code as on 20 04 2018

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+   // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+   // document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
